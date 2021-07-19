6732cb9bf6288786a10d202a2e3bff5f1231a4ff
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-07-19 23:50:35 +0100

    Randomised Food Placement & Added Controls.

--- snake.js
+++ snake.js
@@ -5,14 +5,29 @@ canvas.height = 550;
 
 let headX = 20;
 let headY = 20;
+let p_headX = 425;
+let p_headY = 225;
 let foodX = 21;
 let foodY = 21;
+let p_foodX = Math.random()*canvas.width;
+let p_foodY = Math.random()*canvas.height;
+let speed = 5;
+let leftPressed = false;
+let upPressed = false;
+let rightPressed = false;
+let downPressed = false;
+//EventListners
+document.addEventListener('keydown',movement);
 
 //Game Loop
 function gameLoop(){
     clearCanvas();
+    controls();
     drawPlayer();
     drawFood();
+    requestAnimationFrame(gameLoop);
+
+
 
 }
 
@@ -23,13 +38,59 @@ function clearCanvas(){
 }
 function drawPlayer(){
     context.fillStyle = 'crimson';
-    context.fillRect(0.5*canvas.width,0.5*canvas.height,headX,headY);
+    context.fillRect(p_headX,p_headY,headX,headY);
     context.fill();
 }
 function drawFood(){
     context.fillStyle = 'greenyellow';
-    context.fillRect(0.2*canvas.width,0.2*canvas.height,foodX,foodY);
+    context.fillRect(p_foodX,p_foodY,foodX,foodY);
     context.fill();
+}
+function controls(){
+    if (leftPressed === true){
+        p_headX = p_headX - speed;
+    }
+    if (upPressed === true){
+        p_headY = p_headY - speed; 
+    }
+    if (rightPressed === true){
+        p_headX = p_headX + speed;
+    }
+    if (downPressed === true){
+        p_headY = p_headY + speed;
+    }
+    
+}
+function movement(event){
+    if(event.keyCode == 37){
+        leftPressed = true;
+        rightPressed = false;
+        upPressed =false;
+        downPressed =false;
+        
+        
+    }
+    if(event.keyCode == 38){
+        upPressed = true;
+        downPressed = false;
+        leftPressed = false;
+        rightPressed = false;
+        
+    }
+    if(event.keyCode == 39){
+        rightPressed = true;
+        leftPressed = false;
+        upPressed =false;
+        downPressed =false;
+    }
+    if (event.keyCode == 40){
+        downPressed = true;
+        upPressed = false;
+        leftPressed = false;
+        rightPressed = false;
+        
+        
+    }
 }
 
 gameLoop();
